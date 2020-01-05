34e6f63a449651e7c375f06c0520cfb8ab34e5e1
ducktyping <ducktypings@gmail.com>
2020-01-05 03:03:20 +0300

    added webpack and react

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    modules: ['node_modules', path.join(__dirname, 'src'), 'components'],
+    extensions: ['*', '.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: true, localsConvention: 'camelCaseOnly' }
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    historyApiFallback: true
+  }
+};
